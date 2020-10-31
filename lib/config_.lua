ea4858bf356e86db64542ea588cb39215010efbc
Tyler Etters <tyler@etters.co>
2020-10-31 15:17:07 -0500

    remove config_.lua

--- lib/config_.lua
+++
@@ -1,8 +0,0 @@
-config.settings.dev_mode = true
-config.settings.debug_interpreter = true
-config.settings.dev_scene = 1
-config.settings.load_file = "shift.txt"
-
-if config.settings.dev_mode then
-  table.insert(config.page_titles, "DEV")
-end
