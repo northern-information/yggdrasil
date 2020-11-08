2925392f974022688efe53ff2233104ca7da74dd
Zack Scholl <zack.scholl@gmail.com>
2020-11-08 23:37:18 +0700

    allow moding the default config parameters on startup

--- lib/parameters.lua
+++ lib/parameters.lua
@@ -1,6 +1,7 @@
 parameters = {}
 
 function parameters.init()
+  parameters.startup=true -- prevents writing parameters on startup
 
   params:add_separator("")
   params:add_separator("- Y G G D R A S I L -")
@@ -15,23 +16,38 @@ function parameters.init()
   params:add_option("jf_i2c_tuning", "JF I2C TUNING", {"440 Hz", "432 Hz"})
   params:set_action("jf_i2c_tuning", function(index) crow.ii.jf.god_mode(index == 2 and 1 or 0) end)
 
-  params:add_control("default_depth", "DEFAULT DEPTH", controlspec.new(1,64,"lin",8,1,"rows"))
+  params:add_group("DEFAULTS",3)
+  params:add_control("default_depth", "DEFAULT DEPTH", controlspec.new(1,64,"lin",1,8,"rows"))
   params:set_action("default_depth", function(depth) 
-    config.settings.default_depth == depth
-  	parameters.update()
+    config.settings.default_depth = depth
+    parameters.update()
+  end)
+  params:add_control("default_tracks", "DEFAULT TRACKS", controlspec.new(1,64,"lin",1,8,"tracks"))
+  params:set_action("default_tracks", function(tracks) 
+    config.settings.default_tracks = tracks
+    parameters.update()
+  end)
+  local clades = {"SYNTH","SAMPLER","MIDI","CROW","YPC"}
+  params:add_option("default_clade", "DEFAULT CLADE", clades)
+  params:set_action("default_clade", function(i) 
+    config.settings.default_clade = clades[i]
+    parameters.update()
   end)
 
   if util.file_exists(config.settings.save_path.."yggdrasil.pset") then 
-	params:read(config.settings.save_path.."yggdrasil.pset")
+  	params:read(config.settings.save_path.."yggdrasil.pset")
   else  	
     params:default()
   end
   params:bang()
+  parameters.startup = false
 end
 
 
 function parameters.update()
-  params:write(config.settings.save_path.."yggdrasil.pset")
+  if not parameters.startup then 
+    params:write(config.settings.save_path.."yggdrasil.pset")
+  end
 end
 
 return parameters
